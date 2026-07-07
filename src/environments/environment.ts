96ce3f7e50911543264718e5124c3df71bcf50b7
SebastianPacheco <u202110291@upc.edu.pe>
2026-07-07 11:27:43 -0500

    feat: update api base url to production.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  platformProviderApiBaseUrl: "https://octane-backend.onrender.com/api/v1",
+  platformProviderApiBaseUrl: "https://strong-surprise-production-ef50.up.railway.app/api/v1",
   platformProviderAssignmentsEndpointPath: '/assignments',
   platformProviderMechanicEndpointPath: '/mechanic',
   platformProviderOwnerEndpointPath: '/owner',
